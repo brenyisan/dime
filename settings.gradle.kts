08478979eac800381fe6373fab6764007f6280c1
brenyisan <brenyisan@outlook.com>
2026-07-25 21:09:47 -0400

    Add plugin versions for Android and Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        // Versiones recomendadas: ajusta si necesitas otras
+        id("com.android.application") version "8.1.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,5 +19,6 @@ dependencyResolutionManagement {
         maven { url = uri("https://repo1.maven.org/maven2") }
     }
 }
+
 rootProject.name = "DIME_App"
 include(":app")
